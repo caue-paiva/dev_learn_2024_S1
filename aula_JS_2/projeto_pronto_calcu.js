a2172c294a232c3b89bb01f3ea90a435403eb193
caue <cauepaivalira@outlook.com>
2024-05-12 18:05:25 -0300

    aula 2 de JS e os projetos prontos

---
+++ aula_JS_2/projeto_pronto_calcu.js
@@ -0,0 +1,36 @@
+function calcula(){
+    let valor1 = parseFloat(document.getElementById("val1").value); //.value é usado para acessar valores de inputs
+    let valor2 = parseFloat(document.getElementById("val2").value);
+    
+    let operando = document.getElementById("operador").value;//pega o operador da conta
+    
+    let resultado;
+    
+    switch  (operando){  //switch na operação
+        case  "+":
+            resultado = valor1 + valor2;
+        break;
+            
+        case "-":
+            resultado = valor1 - valor2;
+        break;
+            
+        
+        case "*":
+            resultado = valor1 * valor2;         
+        break;
+        
+        case "/":
+            resultado = valor1 / valor2; 
+        break;    
+        
+        default: //operador inválido
+            resultado = "operador invalido";  
+        break;
+    }
+    
+    
+    let resultado_tag = document.getElementById("resultado"); //pega a tag do resultado
+    resultado_tag.innerText = "Resultado:  " + (resultado.toString()); //muda o inner text dessa tag 
+    
+}
---
+++ aula_JS_2/projeto_pronto_contador.js
@@ -0,0 +1,8 @@
+function incrementa(){
+    let elemento_num = document.getElementById("numero"); //pega elemento html do numero do contador
+    
+    let numero_atual = parseInt(elemento_num.textContent); //transforma a string que veio do elemento html em um int
+    numero_atual++; //incrementa
+    
+    elemento_num.textContent = numero_atual; //atualiza valor do contador no html
+}
